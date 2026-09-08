1a7ddf0f1dec2df757d7bf9683d9306f64a503e4
warlley <warlleysilvax@gmail.com>
2026-09-08 18:20:20 -0300

    Implementa classe Fatura

--- src/Fatura.java
+++ src/Fatura.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class Fatura {
 
+    private ArrayList<Item> itens;
+
+    public Fatura() {
+        itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item) {
+        itens.add(item);
+    }
+
+    public void removerItem(int indice) {
+        itens.remove(indice);
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+
+    public double calcularTotal() {
+
+        double total = 0;
+
+        for (Item item : itens) {
+            total += item.calcularTotal();
+        }
+
+        return total;
+    }
 }
