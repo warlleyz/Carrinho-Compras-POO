8d7cf8f11bd2cc3c0e2750728fcbde4c69c4a471
warlley <warlleysilvax@gmail.com>
2026-09-08 18:19:27 -0300

    Implementa classe Produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,3 +1,24 @@
 public class Produto {
 
+    private String nome;
+    private int codigo;
+    private double preco;
+
+    public Produto(String nome, int codigo, double preco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 }
