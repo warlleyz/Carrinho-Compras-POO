fab00b941523fe6bfc853d24cd3b6b7a77439674
warlley <warlleysilvax@gmail.com>
2026-09-08 18:19:53 -0300

    Implementa classe Item

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,26 @@
 public class Item {
 
+    private Produto produto;
+    private int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal() {
+        return produto.getPreco() * quantidade;
+    }
 }
