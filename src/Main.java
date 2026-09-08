6696519b319b158cb9a08c56ed4c4edd29888310
warlley <warlleysilvax@gmail.com>
2026-09-08 18:21:29 -0300

    Cadastra produtos iniciais

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        ]
+
+        Scanner sc = new Scanner(System.in);
+
+        Produto p1 = new Produto("Teclado", 1, 100.00);
+        Produto p2 = new Produto("Mouse", 2, 50.00);
+        Produto p3 = new Produto("Monitor", 3, 800.00);
+
+        Fatura fatura = new Fatura();
+
     }
 }
